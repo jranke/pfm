7d2096855edcc196629c1c7a9983a56ec6addd1e
Johannes Ranke <jranke@uni-bremen.de>
2015-04-24 02:21:04 +0200

    Add a geometric mean function

---
+++ pkg/R/geomean.R
@@ -0,0 +1,38 @@
+# Copyright (C) 2015  Johannes Ranke
+# Contact: jranke@uni-bremen.de
+# This file is part of the R package pfm
+
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>
+
+#' Calculate the geometric mean
+#'
+#' Based on some posts in a thread on Stackoverflow
+#' \url{http://stackoverflow.com/questions/2602583/geometric-mean-is-there-a-built-in}
+#' This function checks for negative values, removes NA values per default and
+#' returns 0 if at least one element of the vector is 0.
+#'
+#' @param x Vector of numbers
+#' @param na.rm Should NA values be omitted?
+#' @return The geometric mean
+#' @export
+#' @author Johannes Ranke
+#' @examples
+#' geomean(c(1, 3, 9))
+#' geomean(c(1, 3, NA, 9))
+#' \dontrun{geomean(c(1, -3, 9)) # returns an error}
+geomean = function(x, na.rm = TRUE){
+  if (any(is.na(x)) & na.rm == FALSE) stop("Removal of NA values was not requested")
+  if (any(x < 0, na.rm = na.rm)) stop("Only defined for positive numbers")
+  exp(mean(log(x), na.rm = na.rm))
+}
---
+++ pkg/tests/testthat/test_geomean.R
@@ -0,0 +1,11 @@
+library(pfm)
+context("Geometric mean calculation")
+
+test_that("The geometric mean is correctly calculated", {
+  expect_equal(geomean(c(1, 3, 9)), 3)
+  expect_equal(geomean(c(0, 3, 9)), 0)
+  expect_error(geomean(c(1, 3, NA, 9), na.rm = FALSE), "NA")
+  expect_equal(geomean(c(1, 3, NA, 9), na.rm = TRUE), 3)
+  expect_error(geomean(c(1, -3, 9)), "positive")
+  expect_error(geomean(c(1, -3, NA, 9)), "positive")
+})
