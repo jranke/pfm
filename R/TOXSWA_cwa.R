87d63e649da5e12409c50cb06d3d2a01e9880759
Johannes Ranke <jranke@uni-bremen.de>
2017-12-15 17:25:21 +0100

    Improve handling of µ in y axis for plot.TOXSWA_cwa

--- R/TOXSWA_cwa.R
+++ R/TOXSWA_cwa.R
@@ -129,7 +129,13 @@ plot.TOXSWA_cwa <- function(x, time_column = c("datetime", "t", "t_firstjan", "t
                 t_rel_to_max = "Time relative to maximum concentration [days]")
   }
   if (identical(ylab, "default")) {
-    ylab = paste( ifelse(total, "Total concentration", "Concentration"), "[\u03bcg/L]")
+    # Use LateX if the current plotting device is tikz
+    if (names(dev.cur()) == "tikz output") {
+      ylab = paste( ifelse(total, "Total concentration", "Concentration"), "[$\\mu$g/L]")
+    } else {
+      conc_string <- ifelse(total, "Total concentration", "Concentration")
+      ylab = bquote(.(conc_string) ~ group("[", mu*g/L, "]"))
+    }
   }
   if (add) {
     lines(plot_data, xlab = xlab, ylab = ylab, ...)
