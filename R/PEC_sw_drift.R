c97a4f5a9fe30ffc3321681a97eea167cfc427b5
Johannes Ranke <jranke@uni-bremen.de>
2020-07-23 12:22:25 +0200

    PEC_sw_drift: Rautmann formula (1 app, Ackerbau)
    This makes it possible to calculate drift PECsw for other distances than
    present in the JKI data or the Rautmann paper.

--- R/PEC_sw_drift.R
+++ R/PEC_sw_drift.R
@@ -8,7 +8,9 @@
 #' @param applications Number of applications for selection of drift percentile
 #' @param drift_percentages Percentage drift values for which to calculate PECsw.
 #'   'drift_data' and 'distances' if not NULL.
-#' @param drift_data Source of drift percentage data
+#' @param drift_data Source of drift percentage data. If 'JKI', the [drift_data_JKI]
+#'   included in the package is used. If 'RF', the Rautmann formula is used, if
+#'   implemented for the crop type and number of applications
 #' @param crop Crop name (use German names for JKI data), defaults to "Ackerbau"
 #' @param distances The distances in m for which to get PEC values
 #' @param water_depth Depth of the water body in cm
@@ -19,15 +21,23 @@
 #' @author Johannes Ranke
 #' @examples
 #' PEC_sw_drift(100)
-PEC_sw_drift <- function(rate, 
-                         applications = 1,
-                         water_depth = 30, 
-                         drift_percentages = NULL,
-                         drift_data = "JKI",
-                         crop = "Ackerbau",
-                         distances = c(1, 5, 10, 20),
-                         rate_units = "g/ha",
-                         PEC_units = "\u00B5g/L")
+#' # Alternatively, we can use the formula for a single application to "Ackerbau" from the paper
+#' PEC_sw_drift(100, drift_data = "RF")
+#'
+#' # This makes it possible to also use different substances
+#' PEC_sw_drift(100, distances = c(1, 3, 5, 6, 10, 20, 50, 100), drift_data = "RF")
+#'
+#' # Using custom drift percentages is also supported
+#' PEC_sw_drift(100, drift_percentages = c(2.77, 0.95, 0.57, 0.48, 0.29, 0.15, 0.06, 0.03))
+PEC_sw_drift <- function(rate,
+  applications = 1,
+  water_depth = 30,
+  drift_percentages = NULL,
+  drift_data = c("JKI", "RF"),
+  crop = "Ackerbau",
+  distances = c(1, 5, 10, 20),
+  rate_units = "g/ha",
+  PEC_units = "\u00B5g/L")
 {
   rate_units <- match.arg(rate_units)
   PEC_units <- match.arg(PEC_units)
@@ -36,8 +46,19 @@ PEC_sw_drift <- function(rate,
   PEC_sw_overspray <- rate * 1e6 / water_volume          # in µg/L
   dist_index <- as.character(distances)
 
+  RF <- list(
+    "1" = list("Ackerbau" = function(distance) 2.7705 * distance^-0.9787) # p. 134
+  )
+  if (drift_data == "RF") {
+    if (is.null(RF[[as.character(applications)]])) stop("Rautmann formula not included for ", applications, " applications")
+    if (is.null(RF[[as.character(applications)]][[crop]])) stop("Rautmann formula not included for this case")
+  }
+
   if (is.null(drift_percentages)) {
-    drift_percentages <- pfm::drift_data_JKI[[applications]][dist_index, crop]
+    drift_percentages <- switch(drift_data,
+      JKI = pfm::drift_data_JKI[[applications]][dist_index, crop],
+      RF =  RF[[applications]][[crop]](distances)
+    )
     names(drift_percentages) <- paste(dist_index, "m")
   } else {
     names(drift_percentages) <- paste(drift_percentages, "%")
--- tests/testthat/test_PEC_sw_drift.R
+++ tests/testthat/test_PEC_sw_drift.R
@@ -3,13 +3,28 @@ context("Simple PEC surface water calculations with drift entry")
 
 test_that("PEC_sw_drift gives the same results as the CRD PEC calculator", {
   # One application of 30 g/ha to field crops calculated with UK PEC calculator published by CRD
-  expect_equal(round(PEC_sw_drift(30), 3), 
+  expect_equal(round(PEC_sw_drift(30), 3),
                c('1 m' = 0.277, '5 m' = 0.057, '10 m' = 0.029, '20 m' = 0.015))
 
   # 7 applications of 30 g/ha to field crops calculated with UK PEC calculator, initial PEC
-  expect_equal(round(PEC_sw_drift(30, 7), 3), 
+  expect_equal(round(PEC_sw_drift(30, 7), 3),
                c('1 m' = 0.161, '5 m' = 0.033, '10 m' = 0.017, '20 m' = 0.008))
 
-  # 4 applications of 30 g/ha to late fruit crops calculated with UK PEC calculator published by CRD (uses different drift values from SANCO aquatic guidance)
-  #expect_equal(round(PEC_sw_drift(30, 4, crop = "Obstbau spät", distances = c(3, 20, 50)), 3), c('3 m' = 1.101, '20 m' = 0.080, '50 m' = 0.013))
+  # 4 applications of 30 g/ha to late fruit crops calculated with UK PEC
+  # calculator published by CRD. CRD uses different drift values from SANCO aquatic
+  # guidance), except for 50 m
+  pfm_30_4_obst_spaet <- round(PEC_sw_drift(30, 4, crop = "Obstbau spaet",
+      distances = c(3, 20, 50)), 3)
+  crd_30_4_obst_spaet <- c('3 m' = 1.101, '20 m' = 0.080, '50 m' = 0.013)
+  expect_equal(pfm_30_4_obst_spaet[3], crd_30_4_obst_spaet[3])
+})
+
+test_that("The Rautmann formula is correctly implemented", {
+  pfm_jki <- PEC_sw_drift(100)
+  pfm_rf <- PEC_sw_drift(100, drift_data = "RF")
+  expect_equal(pfm_jki, pfm_rf, tolerance = 0.01)
+
+  expect_error(PEC_sw_drift(100, drift_data = "RF", applications = 2), "Rautmann formula not included")
+  expect_error(PEC_sw_drift(100, drift_data = "RF", applications = 1, crop = "Obstbau spaet"),
+    "Rautmann formula not included")
 })
